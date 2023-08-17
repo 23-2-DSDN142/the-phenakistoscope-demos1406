5fd3ed92f97083e3d41afdcc8d8b72a160c52620
Helena <helena.whiteford@outlook.com>
2023-08-17 12:11:15 +1200

    first idea 17/08
    red circle moving in a cool way. inelegant code

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,24 +1,39 @@
 const SLICE_COUNT = 10;
+const backgroundColor = "#ADD8E6"
 
 function setup_pScope(pScope){
-  pScope.output_mode(STATIC_FRAME);
+  pScope.output_mode(ANIMATED_DISK);
+  // STATIC_FRAME  ANIMATED_FRAME  STATIC_DISK  ANIMATED_DISK
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, backgroundColor);  //lets us draw the whole circle background, ignoring the boundaries
+  
+  // var layer1 = new PLayer(faces);
+  // layer1.mode(SWIRL(5));
+  // layer1.set_boundary(200, 1000);
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+  // var layer2 = new PLayer(squares);
+  // layer2.mode( RING );
+  // layer2.set_boundary( 0, 400 );
 
-  var layer2 = new PLayer(squares);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
+  var layer3 = new PLayer(circles);
+  layer3.mode(SWIRL(10));
+  layer3.set_boundary(0, 1000);
+
+  var layer4 = new PLayer(lines);
+  layer4.mode(SWIRL(1));
+  layer4.set_boundary(0, 0);
+
+  var tLayer = new PLayer(topLayer);
+  tLayer.mode(SWIRL(10));
+  tLayer.set_boundary(1000, 3000);
+  
 }
 
 function faces(x, y, animation, pScope){
@@ -41,9 +56,45 @@ function squares(x, y, animation, pScope){
   let backgroundArcEnd = 270 + angleOffset;
 
   fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  arc(x,y,600,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
   fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+  ellipse(0,-300-animation.wave()*50,20) // .wave is a cosine wave btw
+
+}
+
+function circles(x,y,animation,pScope){
+  noStroke()
+  scale(animation.frame*2);
+
+  fill("#FFFF8F")
+  ellipse(-50,0, 20, 20)
+  ellipse(50,0, 20, 20)
+
+}
+
+function lines(x, y, animation, pScope){
+  let angleOffset = (360 / SLICE_COUNT) / 2
+  let backgroundArcStart = 270 - angleOffset;
+  let backgroundArcEnd = 270 + angleOffset;
+  noStroke()
+  // const background = 
+  fill(backgroundColor)
+  ellipse(-20,-20,840,840)
+  // strokeWeight(1)
+  fill(color(255,0,0))
+  ellipse(-20,-20,800,800)
+  // fill(220)
+  // ellipse(-20,-20,800,800)
+
+}
+
+function topLayer(x,y,animation,pScope){
+  noStroke()
+  fill("#FFFF8F")
+  ellipse(400,0,500,300)
+  fill("white")
+  ellipse(0,0,500,300)
+
 
 }
