5486d9614f12b0eee5dc92fbf995bafa7ac2f6cf
Helena <helena.whiteford@outlook.com>
2023-09-01 16:26:14 +1200

    funky circles
    slaylicious

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,46 +1,63 @@
-const SLICE_COUNT = 14;
+const SLICE_COUNT = 10;
 const backgroundColor = "#ADD8E6"
+const circleWidth = 800
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
   // STATIC_FRAME  ANIMATED_FRAME  STATIC_DISK  ANIMATED_DISK
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(false);
+  pScope.draw_layer_boundaries(true);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
 }
 
 function setup_layers(pScope){
+  new PLayer(null, "white");  //lets us draw the whole circle background, ignoring the boundaries
 
-  new PLayer(null, backgroundColor);  //lets us draw the whole circle background, ignoring the boundaries
-  
-  // var layer1 = new PLayer(faces);
-  // layer1.mode(SWIRL(5));
-  // layer1.set_boundary(200, 1000);
+  var layer1 = new PLayer(circles);
+  layer1.mode(SWIRL(5));
+  layer1.set_boundary(400, 2000);
 
-  var layer1 = new PLayer(centrePlanet)
-  layer1.mode(RING)
-  layer1.set_boundary(200,200)
-  
-  var layer2 = new PLayer(circles)
-  layer2.mode(RING)
-  // layer2.set_boundary(500,)
-}
 
-function centrePlanet(x, y, animation, pScope){
-  
-  // scale(animation.frame*2);
-
-  ellipse(0,0,150,150); // draw head
-  fill(30);
-  ellipse(-20,-20,35,35); //draw eye
-
+  // var layer2 = new PLayer(squares);
+  // layer2.mode(RING)
 }
 
 function circles(x, y, animation, pScope){
-  fill("blue")
-  // ellipse(0,-800,150,150)
-  // ellipse(-800, -200-animation.wave()*2500, 150, 150);
-  
+  noStroke()
+  fill("#FFF638")
+  ellipse(x,y,circleWidth)
+  fill("#FFC038")
+  ellipse(50,y,circleWidth)
+  fill("#E07509")
+  ellipse(100,y,circleWidth)
+  fill("#E05109")
+  ellipse(150,y,circleWidth)
+  fill("#E02909")
+  ellipse(150,y,circleWidth)
+  fill("#A60000")
+  ellipse(200,y,circleWidth)
 }
+
+
+
+
+
+
+// background arc
+function squares(x, y, animation, pScope){
+
+  // this is how you set up a background for a specific layer
+  let angleOffset = (360 / SLICE_COUNT) / 2
+  let backgroundArcStart = 270 - angleOffset;
+  let backgroundArcEnd = 270 + angleOffset;
+
+  fill("white")
+  arc(x,y,1500,1500,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+
+  fill(255)
+  // rect(-10-animation.wave()*50,-300,20,20) // .wave is a cosine wave btw
+
+}
+
 
