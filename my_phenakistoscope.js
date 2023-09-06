8fee5f3f8b301a551480533f0e9ed56b7435deff
Helena <helena.whiteford@outlook.com>
2023-09-07 10:39:12 +1200

    commit 7/09
    uhhhh

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,37 +1,83 @@
 const SLICE_COUNT = 12;
 const backgroundColor = "#ADD8E6"
 
+const colors = ["red", "green", "blue", "yellow", "orange"];
+const randomIndex = random(colors.length);
+
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
   // STATIC_FRAME  ANIMATED_FRAME  STATIC_DISK  ANIMATED_DISK
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
-  pScope.load_image("heart_circle" , "png");
+  pScope.load_image("eye" , "png");
 }
 
 function setup_layers(pScope){
-  new PLayer(null, "white");  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, "#f56e76");  //lets us draw the whole circle background, ignoring the boundaries
 
-  // var Layer = new newPLayer(imageTest)
-  // Layer.mode(SWIRL(1))
 
-  var test3Layer = new PLayer(test3)
-  test3Layer.mode(SWIRL(5))
-  test3Layer.set_boundary(0,200)
+  var outerLayer = new PLayer(outerRing)
+
+
+  var test2Layer = new PLayer(test2)
+  test2Layer.mode(SWIRL(7))
+  test2Layer.set_boundary(0,325)
+
+  new PLayer(cutout)
+
+
+  var eyeLayer = new PLayer(imageTest)
+  eyeLayer.mode(RING);
+  eyeLayer.set_boundary(0, 300);
+
+}
+
+function outerRing(x, y, animation, pScope){
+
+}
+
+function cutout(x,y,animation,pScope){
+  strokeWeight(40)
+  noFill()
+  circle(0,0,700)
+
+  fill("black")
+  circle(0,0,250)
+}
+
+function cCircle(x,y,animation,pScope){
+  fill("black")
+
+  push()
+  rotate(90 * animation.frame)
+  let rectJump = 750 + (animation.wave(1) * 50)
+  ellipse(150, 750, 100,100)
+  pop()
 }
 
 function test3(x,y,animation,pScope){
   // let ballSize  = 100 + (animation.wave(1)* 20)
+  push()
   scale(animation.frame*30);
-  noStroke()
-  fill(random(255), random(255), random(255))
-  ellipse(25,0,20); 
+  noFill()
+  stroke(random(255), random(255), random(255))
+  // fill(colors[1]);
+  ellipse(0,0,30); 
+  pop()
+
+  
+
 }
 
 function imageTest(x,y,animation,pScope){
-  pScope.draw_image("heart_circle",x,y);
+  push()
+  scale(1.8)
+  if(animation.frame == 0){
+    pScope.draw_image("eye",x,y);
+  }
+  pop()
 }
 
 function test(x, y, animation, pScope){
@@ -50,7 +96,7 @@ function test(x, y, animation, pScope){
 function test2(x, y, animation, pScope){
   scale(animation.frame*2);
   noStroke()
-  fill("#FAE")
+  fill("#e8dcdd")
   ellipseSize = 80
   ellipseHeight = 20
 
@@ -75,7 +121,7 @@ function circles(x, y, animation, pScope){
   noStroke()
 
   fill("#8B5FBF")
-  ellipse(x,y,circleWidth)
+  ellipse(x,y,1600)
   fill("#7B4FA8")
   ellipse(75,25,1400)
   fill("#6B3F91")
@@ -158,12 +204,7 @@ pop()
 // effect.set_boundary( 0, 1000 );
 
 function Spin(x, y, animation, pScope){
-  push()
-  scale(3)
-  if(animation.frame == 0){
-    // pScope.draw_image("spin",x,y);
-  }
-  pop()
+  
   // translate(50 * animation.frame, 0);
   // scale(animation.frame*2);
   // fill(196, 252, 255)
@@ -193,12 +234,12 @@ function dots(x, y, animation, pScope){
     // ellipse(-20,-20,800,800)
   }
 
-  function circles(x,y,animation,pScope){
-    noStroke()
-    scale(animation.frame*2);
+  // function circles(x,y,animation,pScope){
+  //   noStroke()
+  //   scale(animation.frame*2);
   
-    fill("#FFFF8F")
-    ellipse(-50,0, 20, 20)
-    ellipse(50,0, 20, 20)
+  //   fill("#FFFF8F")
+  //   ellipse(-50,0, 20, 20)
+  //   ellipse(50,0, 20, 20)
   
-  }
+  // }
